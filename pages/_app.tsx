c56252f5e770aa86f42e670aa13884a92daf8e45
PedroMarquetti <pedro.marquettti@gmail.com>
2022-05-03 15:11:01 -0300

    all pages will now show footer and head tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,36 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+const Footer = () => {
+	return (
+		<>
+			<footer>
+				<a href='/about'>about page</a>
+			</footer>
+		</>
+	);
+};
+const DefaultHeader = () => {
+	return (
+		<Head>
+			<title>Pedro Marquetti&apos;s Github Pages</title>
+			<meta name='description' content='My github pages made with NextJS' />
+			<meta content='text/html;charset=UTF-8' />
+			<meta name='author' content='Pedro H L Marquetti' />
+			<link rel='icon' href='./favicon.ico' />
+		</Head>
+	);
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<DefaultHeader />
+			<Component {...pageProps} />
+			<Footer />
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
