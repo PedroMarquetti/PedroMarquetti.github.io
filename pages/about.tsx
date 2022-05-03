99e2e72b5552296e6d2a80daeadaeb9eff780f9f
PedroMarquetti <pedro.marquettti@gmail.com>
2022-05-03 15:13:50 -0300

    updated about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,9 @@
+import styles from "../styles/About.module.css";
 export default function About(props: any) {
 	return (
-		<div className='about-container'>
-			<h1>This page will have some info about me</h1>
+		<div className={styles.main}>
+			<h1 className={styles.title}>This Page will contain info about me!</h1>
+			<h2>This is still a Work in Proggress too</h2>
 		</div>
 	);
 }
