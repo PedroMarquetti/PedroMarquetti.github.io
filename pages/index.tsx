7459a55f22bb96d3b99df289a6d11b2627a89ac0
PedroMarquetti <pedro.marquettti@gmail.com>
2022-05-03 15:11:34 -0300

    updated main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image, { ImageLoader } from "next/image";
 import styles from "../styles/Home.module.css";
 
 function imageLoader({ src }: { src: any }): any {
@@ -8,35 +6,11 @@ function imageLoader({ src }: { src: any }): any {
 }
 const Home: NextPage = () => {
 	return (
-		<div className={styles.container}>
-			<Head>
-				<title>Pedro Marquetti</title>
-				<meta name='description' content='My github pages made with NextJS' />
-				<link rel='icon' href='./favicon.ico' />
-			</Head>
-
-			<main className={styles.main}>
+		<div>
+			<div className={styles.main}>
 				<h1 className={styles.title}>Welcome to my Next.JS github pages!</h1>
 				<h2>This is still a Work in Proggress</h2>
-			</main>
-
-			<footer className={styles.footer}>
-				<a
-					href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-					target='_blank'
-					rel='noopener noreferrer'>
-					Powered by{" "}
-					<span className={styles.logo}>
-						<Image
-							loader={imageLoader}
-							src='/vercel.svg'
-							alt='Vercel Logo'
-							width={72}
-							height={16}
-						/>
-					</span>
-				</a>
-			</footer>
+			</div>
 		</div>
 	);
 };
