e756a86c6133bede2ba669b97304393cb08ac94a
pedromarquetti <pedro.marquettti@gmail.com>
2022-07-06 16:02:36 -0300

    added content to about/dev/

--- pages/about/dev.tsx
+++ pages/about/dev.tsx
@@ -1,7 +1,40 @@
 export default function Dev() {
 	return (
-		<>
-			<h1>Soon!</h1>
-		</>
+		<main>
+			<h1>I love coding and computers</h1>
+			<h2>
+				I learned coding because of{" "}
+				<a title='Mr Robot' href='https://mrrobot.fandom.com/wiki/Mr._Robot'>
+					Mr Robot.
+				</a>
+			</h2>
+			<p>
+				First I learned Python, just for fun, then I started &apos;hacking&apos;
+				with{" "}
+				<a title='Try Hack Me' href='https://tryhackme.com/'>
+					Try hack Me
+				</a>
+				, It is a lot of fun, go check it out! Also learned a lot from{" "}
+				<a
+					title='John Hammond'
+					href='https://www.youtube.com/channel/UCVeW9qkBjo3zosnqUbG7CFw'>
+					John Hammond
+				</a>{" "}
+				and his walkthroughs.
+			</p>
+			<p>
+				Then I decided to learn JS + other languages, and since I like to learn
+				by myself, I used{" "}
+				<a title='SoloLearn' href='https://www.sololearn.com/'>
+					SoloLearn
+				</a>
+				, great app, go check it out.
+			</p>
+			<p>
+				Since I&apos;m self taught, I am aware that I might not write the
+				cleanest of codes, so, please go yell at me on my{" "}
+				<a href='https://github.com/pedromarquetti/'>GitHub profile</a>
+			</p>
+		</main>
 	);
 }
