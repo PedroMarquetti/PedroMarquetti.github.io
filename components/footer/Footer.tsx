6f21571ce3435d8774484b712b1acd56da905193
pedromarquetti <pedro.marquettti@gmail.com>
2024-01-16 22:36:51 -0300

    removed images
    using BlueprintJS icons

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,9 +1,6 @@
 import Link from "next/link";
-import Image from "next/image";
-import GithubLogo from "../../public/images/GitHub-Mark-32px.png";
-import Mail from "../../public/images/mail.png";
-import Info from "../../public/images/info.png";
-import Home from "../../public/images/home.png";
+import { Icon } from "@blueprintjs/core";
+import { GitBranch, SendMessage, InfoSign, Home } from "@blueprintjs/icons";
 
 export default function Footer() {
 	const myLoader = ({ src }: any) => {
@@ -16,45 +13,25 @@ export default function Footer() {
 					title='My Github profile'
 					href={"https://github.com/pedromarquetti/"}
 					target='_blank'
-					rel='noreferrer'>
-					<Image
-						loader={myLoader}
-						unoptimized={true}
-						src={GithubLogo}
-						alt='Visit me on github'></Image>
+					rel='noreferrer'
+				>
+					<Icon icon={<GitBranch size={20} />} />
 					<p>My Github profile</p>
 				</a>
 				<a title='Mail me!' href='mailto:pedro.marquettti@gmail.com'>
-					<Image
-						loader={myLoader}
-						unoptimized={true}
-						src={Mail}
-						width={32}
-						height={32}
-						alt='Mail me!'></Image>
+					<Icon icon={<SendMessage size={20} />} />
 					<p>Mail me!</p>
 				</a>
 				<Link href={"/about"}>
 					<a title='About me'>
-						<Image
-							loader={myLoader}
-							unoptimized={true}
-							src={Info}
-							width={32}
-							height={32}
-							alt='About'></Image>
+						<Icon icon={<InfoSign size={20} />} />
+
 						<p>About me</p>
 					</a>
 				</Link>
 				<Link href={"/"}>
 					<a title='Home'>
-						<Image
-							loader={myLoader}
-							unoptimized={true}
-							src={Home}
-							width={32}
-							height={32}
-							alt='Go home'></Image>
+						<Icon icon={<Home size={20} />} />
 						<p>Home</p>
 					</a>
 				</Link>
