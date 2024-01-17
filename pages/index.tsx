e38b8ba0742b9d348545e792f43925c3872349c6
pedromarquetti <pedro.marquettti@gmail.com>
2024-01-16 22:42:41 -0300

    add some info on index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
+import { Button } from "@blueprintjs/core";
 
 function imageLoader({ src }: { src: any }): any {
 	return src;
@@ -9,49 +10,14 @@ const Home: NextPage = () => {
 		<div>
 			<div className={styles.main}>
 				<h1 className={styles.title}>Welcome to my Next.JS github pages!</h1>
-				<h2>This is still a Work in Proggress</h2>
 				<p>
 					I have a degree in biomedicine and can code{" "}
-					<code>print(&quot;hello world&quot;)</code>{" "}
+					<code>print(&quot;hello world&quot;)</code> , currently studying
+					Information Technology.
 				</p>
 				<p>
 					I&apos;ll post here some basic projects, some info, maybe some
 					tutorials? who knows... take a look around
-				</p>
-				<p>
-					The{" "}
-					<a
-						target='_blank'
-						rel='noreferrer'
-						href='https://www.flaticon.com/free-icons/mail'
-						title='Mail icons created by Freepik - Flaticon'>
-						&quot;Mail icon&quot;
-					</a>
-					,{" "}
-					<a
-						target='_blank'
-						rel='noreferrer'
-						href='https://www.flaticon.com/free-icons/about'
-						title='About icons created by Tempo_doloe - Flaticon'>
-						&quot;About icon&quot;
-					</a>{" "}
-					and{" "}
-					<a
-						target='_blank'
-						rel='noreferrer'
-						href='https://www.flaticon.com/free-icons/home'
-						title='Home icons created by Aswell Studio - Flaticon'>
-						&quot;Home icon&quot;
-					</a>{" "}
-					can be found at{" "}
-					<a
-						target='_blank'
-						rel='noreferrer'
-						href='https://www.flaticon.com'
-						title='Flat icons'>
-						Flat Icon
-					</a>{" "}
-					website
 				</p>
 			</div>
 		</div>
