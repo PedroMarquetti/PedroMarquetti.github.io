497fdc454c2ef04b5d1ee8479cc6b10ca85b777b
pedromarquetti <pedro.marquettti@gmail.com>
2024-02-24 22:59:28 -0300

    added some text

--- pages/about/biomedicine.tsx
+++ pages/about/biomedicine.tsx
@@ -1,7 +1,18 @@
 export default function Biomedicine() {
 	return (
 		<>
-			<h1>Soon!</h1>
+			<h1>I like how the human body works too!</h1>
+
+			<h2>I have a bachelor&apos;s in biomedicine</h2>
+			<p>
+				I find it fascinating how the human body develops (also because I have
+				low vision), that is why I did research on the role of PAX6 during eye
+				development.
+			</p>
+			<p>
+				basically this gene is responsible for retina development, since I have
+				a PAX6 genetic mutation, my FOV is messed up (my focus is not centered)
+			</p>
 		</>
 	);
 }
